88be889826bd764e95c2bb249ba7056da46dd0a4
i-tatte <i.tatte.informatics@gmail.com>
2021-10-29 15:02:47 +0900

    プロジェクト ファイルを追加します。

---
+++ MetronomeBeat/CsvOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace MetronomeBeat {
+    internal static class CsvOut {
+        internal static void write(List<DateTime>[] lines) {
+            string path = @".\output" + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + ".csv";
+            FileStream fs = File.Create(path);
+            fs.Close();
+            StreamWriter writer = new StreamWriter(path, false, Encoding.UTF8);
+            for(int i = 0; i < Math.Max(Math.Max(lines[0].Count, lines[1].Count), lines[2].Count); i++) {
+                string first = lines[0].Count > i ? lines[0][i].ToString("ss.fff") : "9999";
+                string second = lines[1].Count > i ? lines[1][i].ToString("ss.fff") : "9999";
+                string third = lines[2].Count > i ? lines[2][i].ToString("ss.fff") : "9999";
+                writer.WriteLine(string.Format("{0},{1},{2}", first, second, third));
+                Debug.WriteLine(string.Format("{0},{1},{2}", first, second, third));
+            }
+            writer.Close();
+        }
+    }
+}
---
+++ MetronomeBeat/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using NAudio.Wave;
+
+namespace MetronomeBeat {
+    public partial class MainWindow : Window {
+
+        private System.Media.SoundPlayer player = null;
+        WaveOut player2 = new WaveOut();
+        WaveOut player3 = new WaveOut();
+        private readonly string ClickSoundFile = "click.wav";
+        private readonly string FeedbackSoundFile = "feedback.wav";
+        private bool makeClickSound = false;
+        private bool makeFeedbackSound = false;
+        private List<DateTime>[] outputTimeList = new List<DateTime>[3];
+
+        private DispatcherTimer _timer;
+        private DateTime prevPlayedTime = DateTime.Now;
+        private readonly double bpm = 120;
+
+        private bool monitoring = false;
+
+        public MainWindow() {
+            InitializeComponent();
+            InitializeTimer();
+            player2.Init(new AudioFileReader(FeedbackSoundFile));
+            player2.PlaybackStopped += (s, e) => {
+                player2.Init(new AudioFileReader(FeedbackSoundFile));
+            };
+            player3.Init(new AudioFileReader(FeedbackSoundFile));
+            player3.PlaybackStopped += (s, e) => {
+                player3.Init(new AudioFileReader(FeedbackSoundFile));
+            };
+            for (int i = 0; i < 3; i++) {
+                outputTimeList[i] = new List<DateTime>();
+            }
+        }
+
+        private void PlayClickSound() {
+            player = new System.Media.SoundPlayer(ClickSoundFile);
+            player.Play();
+        }
+        private void PlayFeedbackSound(int a) {
+            if (a == 0) player2.Play();
+            else player3.Play();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            if (monitoring) {
+                monitoring = false;
+                button.Content = "計測開始";
+                text.Text = "状態：待機中";
+                CsvOut.write(outputTimeList);
+            } else {
+                monitoring = true;
+                button.Content = "計測停止";
+                text.Text = "状態：計測中";
+                outputTimeList = new List<DateTime>[3];
+                for (int i = 0; i < 3; i++) {
+                    outputTimeList[i] = new List<DateTime>();
+                }
+            }
+        }
+        private void TimerMethod() {
+            if ((DateTime.Now - prevPlayedTime).TotalMinutes > 1 / bpm) {
+                prevPlayedTime += new TimeSpan(0, 0, 0, 0, (int)(60000 / bpm));
+                if (makeClickSound) PlayClickSound();
+                outputTimeList[0].Add(DateTime.Now);
+            }
+        }
+
+        private void InitializeTimer() {
+            _timer = new DispatcherTimer(DispatcherPriority.Send) {
+                Interval = new TimeSpan(0, 0, 0, 0, 10),
+            };
+            _timer.Tick += (e, s) => { TimerMethod(); };
+            this.Closing += (e, s) => { _timer.Stop(); };
+            prevPlayedTime = DateTime.Now;
+            // PlaySound();
+            _timer.Start();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) {
+            makeClickSound = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e) {
+            makeClickSound = false;
+        }
+
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e) {
+            makeFeedbackSound = true;
+        }
+
+        private void CheckBox_Unchecked_1(object sender, RoutedEventArgs e) {
+            makeFeedbackSound = false;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Key != Key.A && e.Key != Key.L) return;
+            if (makeFeedbackSound) PlayFeedbackSound(e.Key == Key.A ? 1 : 0);
+            if (!monitoring) return;
+            if(e.Key == Key.A) {
+                outputTimeList[1].Add(DateTime.Now);
+            } else {
+                outputTimeList[2].Add(DateTime.Now);
+            }
+        }
+    }
+
+}
